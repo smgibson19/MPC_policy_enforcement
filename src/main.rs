9681e7c6a5c9efaddd1fa7671a2867ce7be81e0f
smgibson19 <smjgibson17@gmail.com>
2025-10-26 19:56:59 -0400

    start implement share

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 // function for share
+fn share(data: i32, shares: i32){
+    
+    let mut poly = Vec::new(); // where coefficients are saved
+    let mut num_shares = Vec::new(); // where 
 
+
+    // calc polynomials
+    for x in 0..shares-1{
+        random = 
+    }
+
+
+}
 
 // function for operation on data
 
