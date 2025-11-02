6dac02b214ac65168ee2cc51dd7bccc7d25eccf4
smgibson19 <smjgibson17@gmail.com>
2025-11-01 20:44:11 -0400

    start implement share()

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
-// function for share
-fn share(data: i32, shares: i32){
+use rand::Rng;
+
+// function that creates secret shares
+// data is what is being shared
+// shares is how many parties the data is shared between
+fn share(data: usize, shares: usize) -> Vec<usize> {
     
-    let mut poly = Vec::new(); // where coefficients are saved
-    let mut num_shares = Vec::new(); // where 
+    let mut split: Vec<usize> = Vec::with_capacity(shares); 
+    let mut sum = 0;
 
 
     // calc polynomials
-    for x in 0..shares-1{
-        random = 
+    for x in 0..shares{
+        // generate random number
+        // add it to sum
+        // push it to shares
+        let mut rng = rand::thread_rng();      
+        let num: usize = rng.gen();
+
+        sum += num * 100;
+        split.push(sum);
+
     }
 
+    // add x- sum to shares
+    split.push(data-sum);
+
+    return split;
 
 }
 
