542b8c41c82366a8eb3b692111564c498d47aa97
smgibson19 <smjgibson17@gmail.com>
2025-11-02 15:47:21 -0500

    share fn finished test1

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,22 @@ use rand::Rng;
 // function that creates secret shares
 // data is what is being shared
 // shares is how many parties the data is shared between
-fn share(data: usize, shares: usize) -> Vec<usize> {
+fn share(data: i32, shares: i32) -> Vec<i32> {
     
-    let mut split: Vec<usize> = Vec::with_capacity(shares); 
-    let mut sum = 0;
+    let mut split: Vec<i32> = Vec::new(); 
+    let mut sum: i32 = 0;
 
 
     // calc polynomials
-    for x in 0..shares{
-        // generate random number
-        // add it to sum
-        // push it to shares
-        let mut rng = rand::thread_rng();      
-        let num: usize = rng.gen();
+    for _x in 0..shares-1{
+        let num: i32 = rand::thread_rng().gen_range(0..=100);  
+        let mut sign: i32 = rand::thread_rng().gen_range(-1..=1);
 
-        sum += num * 100;
+        if sign == 0{
+            sign += 1;
+        }
+
+        sum += num* sign;
         split.push(sum);
 
     }
@@ -32,5 +33,10 @@ fn share(data: usize, shares: usize) -> Vec<usize> {
 // function for operation on data
 
 fn main(){
-    println!("Hey!");
+    let foo:i32 = 23;
+    let shares = 3;
+
+    let results: Vec<i32> = share(foo, shares);
+
+    println!("{:?}", results);
 }
