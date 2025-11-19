c82a91511324e2b4c27b70dc5122d46d65b1437b
smgibson19 <smjgibson17@gmail.com>
2025-11-19 09:53:41 -0500

    saving to pull riya's code

--- src/mpcClient.rs
+++ src/mpcClient.rs
@@ -2,6 +2,7 @@ use std::io:{ErrorKind, Read, Write};
 use std::net::{TcpListener, TcpStream, Shutdown};
 use std::sync::mpsc;
 use stf::thread;
+use std::env;
 
 // make shares
 fn share(data: i32, shares: i32) -> Vec<i32> {
@@ -31,9 +32,7 @@ fn share(data: i32, shares: i32) -> Vec<i32> {
 }
 
 fn client_process() {
-    // since number of parties is given, split can happen before connection
-
-    let secret1: i32 = 15; // set this secret as an argument from the commandline
+    let secret1: i32 = &args[1]; // set this secret as an argument from the commandline
 
     let num_parties: i32 = 3; // here is given
 
@@ -42,6 +41,39 @@ fn client_process() {
 
     // 2) connect to servers
     
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    match TcpStream::connect("localhost:3333") {
+        Ok(mut stream) => {
+            println!("Successfully connected to server in port 3333");
+
+            let msg = b"Hello!";
+
+            stream.write(msg).unwrap();
+            println!("Sent Hello, awaiting reply...");
+
+            let mut data = [0 as u8; 6]; // using 6 byte buffer
+            match stream.read_exact(&mut data) {
+                Ok(_) => {
+                    if &data == msg {
+                        println!("Reply is ok!");
+                    } else {
+                        let text = from_utf8(&data).unwrap();
+                        println!("Unexpected reply: {}", text);
+                    }
+                },
+                Err(e) => {
+                    println!("Failed to receive data: {}", e);
+                }
+            }
+        },
+        Err(e) => {
+            println!("Failed to connect: {}", e);
+        }
+    }
+    println!("Terminated.");
+}
     // 3) send data to both servers
 }
 
